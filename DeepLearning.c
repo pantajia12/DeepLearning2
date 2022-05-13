f0dfcedbfc71cf40420236d0c04beb75525ab446
한예찬 <pantajia12@naver.com>
2022-05-13 21:46:55 +0900

    Feat: softmax함수 추가

--- DeepLearning.c
+++ DeepLearning.c
@@ -6,7 +6,9 @@ double** forward(double*);
 double** dot(double*, double*, int, int, int);
 double** add(double**, double*, int, int);
 double** sigmoid(double**, int, int);
+double** softmax(double*, int, int);
 double** identity_function(double**);
+double max(double*, int, int);
 
 double W1[2][3] = {{0.1, 0.3, 0.5}, {0.2, 0.4, 0.6}};
 double b1[1][3] = {0.1, 0.2, 0.3};
@@ -27,7 +29,7 @@ double** forward(double* x){
     double** a2 = add((dot(*z1, W2, 1, 3, 2)), b2, 1, 2);
     double** z2 = sigmoid(a2, 1, 2);
     double** a3 = add((dot(*z2, W3, 1, 2, 2)), b3, 1, 2);
-    double** y = identity_function(a3);
+    double** y = softmax(*a3, 1, 2);
     
     return y;
 } 
@@ -51,7 +53,7 @@ double** dot(double* x, double* W, int l, int n, int m){//(x, W1, x[n][], x[][n]
 }
 
 double** add(double** a, double* b, int x, int y){
-	double **result; // x행 y열로 선언할거임
+   double **result; // x행 y열로 선언할거임
 
     result = calloc(sizeof(double*), x);
     result[0] = calloc(sizeof(double), x * y);
@@ -82,6 +84,45 @@ double** sigmoid(double** x, int l, int m){
     return result;
 }
 
+double** softmax(double* x, int l, int m){
+    double **result; // l행 m열로 선언할거임
+
+    result = calloc(sizeof(double*), l);
+    result[0] = calloc(sizeof(double), l * m);
+    for (int i = 1; i < l; ++i) result[i] = result[i - 1] + m;
+
+    double arrMax = max(x, l, m);
+    double sumExpX = 0;
+
+    for(int i = 0; i < l; i++){
+        for(int j = 0; j < m; j++){
+            sumExpX += exp(x[j+i*m] - arrMax);
+        }
+    }
+
+    for(int i = 0; i < l; i++){
+        for(int j = 0; j < m; j++){
+            result[i][j] = exp(x[j+i*m] - arrMax) / sumExpX;
+        }
+    }
+    
+    return result;
+}
+
 double** identity_function(double** x){
     return x;
+}
+
+double max(double* x, int l, int m){
+    double max = x[0];
+
+    for(int i = 0; i < l; i++){
+        for(int j = 0; j < m; j++){
+            if(max < x[j+i*m]){
+                max = x[j+i*m];
+            }
+        }
+    }
+
+    return max;
 }
